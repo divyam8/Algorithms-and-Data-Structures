130047ae497ce247c7ea2f6785dc891327adc215
Divyam Khandelwal <khandelwal.divyam@gmail.com>
2016-04-01 09:47:54 +0530

    Update fib.c

--- DynamicProgramming/fib.c
+++ DynamicProgramming/fib.c
@@ -1,1 +1,20 @@
+#include<stdio.h>
+
+int main(int argc, char const *argv[])
+{
+   int n,a=0,b=1,sum,i;	
+   printf("Enter the no\n");
+   scanf("%d",&n);
+  
+   for(i=0;i<n;i++)
+   {
+   	 sum=a+b;
+   	 a=b;
+   	 b=sum;
+   }
+
+   printf("Fibonacci Number:%d",sum);
+
+   return 0;
+}	
 
